04e7441728210d51192f9967693712944e6cc419
ITxiaohao <shunhaozeng@gmail.com>
2019-07-16 23:09:59 +0800

    refactor(utils.js): 优化获取对应目录下的文件名函数

--- .vuepress/config/utils.js
+++ .vuepress/config/utils.js
@@ -1,13 +1,13 @@
 const fs = require('fs')
 const process = require('process')
-const oldWebpack4Files = fs.readdirSync(process.cwd() + '/webpack4')
 
-function generateWebpack() {
+function generateOrderFileName(path) {
+  const oldFiles = fs.readdirSync(process.cwd() + path)
   let arr = []
-  for (let i = 0; i < oldWebpack4Files.length; i++) {
+  for (let i = 0; i < oldFiles.length; i++) {
     let obj = {}
-    obj.fileName = oldWebpack4Files[i]
-    let index = oldWebpack4Files[i].slice(0, 2)
+    obj.fileName = oldFiles[i]
+    let index = oldFiles[i].slice(0, 2)
     if (index.includes('：')) {
       index = index.slice(0, 1)
     }
@@ -15,12 +15,29 @@ function generateWebpack() {
     arr.push(obj)
   }
 
-  let webpack4Files = []
+  let files = []
   arr.sort(sortNum('index')).filter(v => {
-    webpack4Files.push(v.fileName)
+    files.push(v.fileName)
   })
 
-  return webpack4Files
+  return files
+}
+
+function generateFileName(path) {
+  const oldFiles = fs.readdirSync(process.cwd() + path)
+  let arr = []
+  for (let i = 0; i < oldFiles.length; i++) {
+    let obj = {}
+    obj.fileName = oldFiles[i]
+    let index = oldFiles[i].slice(0, 2)
+    if (index.includes('：')) {
+      index = index.slice(0, 1)
+    }
+    obj.index = Number(index)
+    arr.push(obj)
+  }
+
+  return arr
 }
 
 function sortNum(index) {
@@ -32,5 +49,6 @@ function sortNum(index) {
 }
 
 module.exports = {
-  generateWebpack
+  generateOrderFileName,
+  generateFileName
 }

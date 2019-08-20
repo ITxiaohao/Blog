7e40ee1acad9c04d7226bb0761f38f8a03ec8591
ITxiaohao <shunhaozeng@gmail.com>
2019-08-20 14:07:14 +0800

    chore: webpack 系列文件重命名

--- .vuepress/config/sidebar.js
+++ .vuepress/config/sidebar.js
@@ -1,5 +1,6 @@
-const { generateFileName } = require('./utils')
-// console.log('webpack4', generateOrderFileName('/webpack4'))
+const { generateFileName, generateOrderFileName } = require('./utils')
+
+let webpack4Files = generateOrderFileName('/webpack4')
 
 let leetCodeEasy = generateFileName('/数据结构与算法/leetCode/easy').map(
   v => `easy/${v}`
@@ -65,33 +66,7 @@ const webpack4 = [
   {
     title: 'webpack4',
     collapsable: false,
-    children: [
-      '0：前言.md',
-      '1：搭建项目并打包 JS 文件.md',
-      '2：生产和开发模式.md',
-      '3：覆盖默认 entry、output.md',
-      '4：用 Babel7 转译 ES6.md',
-      '5：Code Splitting.md',
-      '6：Lazy Loading、Prefetching.md',
-      '7：自动生成 HTML 文件.md',
-      '8：处理 CSS、SCSS 文件.md',
-      '9：JS Tree Shaking.md',
-      '10：CSS Tree Shaking.md',
-      '11：图片处理汇总.md',
-      '12：字体文件处理.md',
-      '13：处理第三方 JS 库.md',
-      '14：开发模式与 webpack-dev-server.md',
-      '15：开发模式和生产模式・实战.md',
-      '16：打包自定义函数库.md',
-      '17：PWA 配置.md',
-      '18：TypeScript 配置.md',
-      '19：Eslint 配置.md',
-      '20：使用 DLLPlugin 加快打包速度.md',
-      '21：多页面打包配置.md',
-      '22：编写 loader.md',
-      '23：编写 plugin.md',
-      '24：编写 Bundle.md'
-    ]
+    children: webpack4Files
   }
 ]
 
